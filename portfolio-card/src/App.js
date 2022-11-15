612199ab9a61eb396aa57f06b6a6ab2478735352
erwinagpasa <istran.net@gmail.com>
2022-11-15 21:07:25 +0300

    fixing gaps

--- portfolio-card/src/App.js
+++ portfolio-card/src/App.js
@@ -1,72 +1,74 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// import { faHouse } from '@fortawesome/free-solid-svg-icons'
+
+import {  faGithub } from '@fortawesome/free-brands-svg-icons'
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
- 
+        <div className="container-fluid col-md-9 py-5">
 
 
-<div className="container-fluid col-md-9 py-5">
-        <div className="container">
             <div className="row">
-                <div className="col-lg-7 portfolio-img">
-                <Link to="/"> 
-                <img className="w-100" alt="" src="img/ccs.jpeg" />
-                </Link>
-                </div>
+              <div className="col-lg-7 portfolio-img">
+              <Link to="/"><img className="w-100" alt="" src="img/ccs.jpg" /></Link>
+              </div>
                 
-                <div className="col-lg-5 portfolio-wrapper">
-                  <small className="d-block text-white text-end">Featured Project</small> 
+              <div className="col-lg-5 portfolio-wrapper">
+                <small className="d-block text-white text-end">Featured Project</small> 
                   <h4 className="text-white text-end">Halcyon Theme</h4>
-                    <div className="text-wrapper">
-                        <div className="text-inner">
+                    <div className="description">
+                      <div className="text-inner">
                         <p>A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, 
                           and more. Available on Visual Studio Marketplace, Package Control, 
-                          Atom Package Manager, and npm.
-                        </p>
-                        </div>
-                        <p className="text-end">VS Code, Sublime Text, Atom, iTerm2, Hyper</p>
-                    </div>
-
-                   
-                </div>
+                          Atom Package Manager, and npm.</p>
+                      </div>
+                        <ul className="list-group list-group-horizontal list-group-flush justify-content-end right-list">
+                          <li className="list-group-item bg-transparent">ReactJS</li>
+                          <li className="list-group-item bg-transparent">Express</li>
+                          <li className="list-group-item bg-transparent">MySQL</li>
+                          <li className="list-group-item bg-transparent">Netlify</li>
+                        </ul>
+                        <div className="text-end"><Link to="\"><FontAwesomeIcon icon={faGithub} /></Link></div>
+                    </div>                   
+              </div>
             </div>
-        </div>
-    </div>
+       
 
 
 
-{/* 
-
-    <div className="container-fluid py-5">
-        <div className="container pt-5">
             <div className="row">
-            <div className="col-lg-6 pt-5 pb-lg-5">
-                    <div className="text-right bg-white p-4 p-lg-5 my-lg-2">
-                        <h2 className="mb-3">Quest International Travel & Tours</h2>
-                        <p>
-                        As we strive for good service motivated by our mission to establish successful, 
-                        long-term, and great connections with our clients and opening doors for travel 
-                        opportunities to individuals throughout the world, quality and cost 
-                        go hand in hand.
-                        </p>
-                    </div>
-                    <p className="text-white">VS Code, Sublime Text, Atom, iTerm2, Hyper</p>
-                </div>
+                <div className="col-lg-5 portfolio-wrapper">
+                <small className="d-block text-white">Featured Project</small> 
+                  <h4 className="text-white">Halcyon Theme</h4>
+                    <div className="description-left">
+                      <div className="text-inner">
+                        <p>A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, 
+                          and more. Available on Visual Studio Marketplace, Package Control, 
+                          Atom Package Manager, and npm.</p>
+                      </div>
+                        <ul className="list-group list-group-horizontal list-group-flush justify-content-start left-list">
+                          <li className="list-group-item bg-transparent">ReactJS</li>
+                          <li className="list-group-item bg-transparent">Express</li>
+                          <li className="list-group-item bg-transparent">MySQL</li>
+                          <li className="list-group-item bg-transparent">Netlify</li>
+                        </ul>
+                        <div className="text-start"><Link to="\"><FontAwesomeIcon icon={faGithub} /></Link></div>
+                    </div>                   
+              </div>
 
-                <div className="col-lg-6">
-                    <div className="position-relative h-100">
-                        <img className="position-absolute w-100 h-100 card-img" alt="" src="img/couple.jpg" />
-                    </div>
-                </div>
-                
+              <div className="col-lg-7 portfolio-img">
+              <Link to="/"><img className="w-100" alt="" src="img/mob.jpg" /></Link>
+              </div>
             </div>
+
         </div>
-    </div> */}
-
-
       </header>
     </div>
   );
