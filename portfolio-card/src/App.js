93139e11a012dcb498ee53d1d25f2a6ef7b5fa3a
erwinagpasa <istran.net@gmail.com>
2022-11-15 16:23:58 +0300

    fixing responsive card

--- portfolio-card/src/App.js
+++ portfolio-card/src/App.js
@@ -1,22 +1,72 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+ 
+
+
+<div className="container-fluid col-md-9 py-5">
+        <div className="container pt-5">
+            <div className="row">
+                <div className="col-lg-7 g-0 bg-white imahe">
+                    <div className="xposition-relative">
+                        <img className="w-100 card-img" alt="" src="img/halcyon.png" />
+                    </div>
+                </div>
+                
+                <div className="col-lg-5 text-card">
+                  <small className="text-white d-block text-end">Featured Project</small> 
+                  <h4 className="text-white text-end">Halcyon Theme</h4>
+                    <div className="about-text my-lg-2">
+                        <div className="p-4 p-lg-4 bg-white">
+                        <p>A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, 
+                          and more. Available on Visual Studio Marketplace, Package Control, 
+                          Atom Package Manager, and npm.
+                        </p>
+                        </div>
+                        <p className="text-white text-end">VS Code, Sublime Text, Atom, iTerm2, Hyper</p>
+                    </div>
+
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+{/* 
+
+    <div className="container-fluid py-5">
+        <div className="container pt-5">
+            <div className="row">
+            <div className="col-lg-6 pt-5 pb-lg-5">
+                    <div className="text-right bg-white p-4 p-lg-5 my-lg-2">
+                        <h2 className="mb-3">Quest International Travel & Tours</h2>
+                        <p>
+                        As we strive for good service motivated by our mission to establish successful, 
+                        long-term, and great connections with our clients and opening doors for travel 
+                        opportunities to individuals throughout the world, quality and cost 
+                        go hand in hand.
+                        </p>
+                    </div>
+                    <p className="text-white">VS Code, Sublime Text, Atom, iTerm2, Hyper</p>
+                </div>
+
+                <div className="col-lg-6">
+                    <div className="position-relative h-100">
+                        <img className="position-absolute w-100 h-100 card-img" alt="" src="img/couple.jpg" />
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div> */}
+
+
       </header>
     </div>
   );
